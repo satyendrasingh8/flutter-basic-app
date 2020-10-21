88582c452836da6dc71040e440cb659d61f95f73
satyendrasingh8 <satyendrasinghsss7398@gmail.com>
2020-10-22 00:25:26 +0530

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -12,17 +12,7 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  var _questionIndex = 0;
-  void _answerQuestion() {
-    setState(() {
-      _questionIndex += 1;
-    });
-    print('object chosen');
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    var questions = [
+  final questions = const [
       {
         'questionText': 'What\'s your favourite color? ',
         'answers': ['black', 'red', 'green', 'white']
@@ -36,20 +26,31 @@ class _MyAppState extends State<MyApp> {
         'answers': ['Max', 'Max2', 'Max3', 'Max4']
       },
     ];
+  var _questionIndex = 0;
+  void _answerQuestion() {
+    setState(() {
+      _questionIndex += 1;
+    });
+    print('object chosen');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+   
 
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('AppBar'),
         ),
-        body: Column(
+        body:(_questionIndex < questions.length) ? Column(
           children: [
             Question(questions[_questionIndex]['questionText']),
             ...(questions[_questionIndex]['answers'] as List<String>).map((answer) {
               return Answer(_answerQuestion,answer);
             }).toList()
-          ],
-        ),
+          ], 
+        ) : Center(child:Text('you did it',style: TextStyle(fontSize: 30.0),))
       ),
     );
   }
